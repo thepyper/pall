cf8a4c2d215d40dd660986c4ba3865029a6f54a1
thepyper <thepyper@users.noreply.github.com>
2026-04-28 16:07:03 +0200

    more aprse

--- src/machine/expression.rs
+++ src/machine/expression.rs
@@ -75,12 +75,24 @@ struct ExpressionParser;
 #[test]
 fn test_parse_expression()
 {
-    eprintln!("1. {:?}", ExpressionParser::parse(Rule::expression, r###"1"###).unwrap());
-    eprintln!("2. {:?}", ExpressionParser::parse(Rule::expression, r###"(1)"###).unwrap());
-    eprintln!("3. {:?}", ExpressionParser::parse(Rule::expression, r###"1 + 2"###).unwrap());
-    eprintln!("4. {:?}", ExpressionParser::parse(Rule::expression, r###"(1 + 3)"###).unwrap());
-    eprintln!("5. {:?}", ExpressionParser::parse(Rule::expression, r###"1 + (4)"###).unwrap());
-    eprintln!("6. {:?}", ExpressionParser::parse(Rule::expression, r###"1 + (5 + 6)"###).unwrap());
-    eprintln!("7. {:?}", ExpressionParser::parse(Rule::expression, r###"(1 + 7) + (8 - 9)"###).unwrap());
-    eprintln!("8. {:?}", ExpressionParser::parse(Rule::expression, r###"(1 + (2 + (3 + 4))) + 8"###).unwrap());
+    eprintln!("1. {:?}", ExpressionParser::parse(Rule::expression,  r###"1"###).unwrap());
+    eprintln!("2. {:?}", ExpressionParser::parse(Rule::expression,  r###"(1)"###).unwrap());
+    eprintln!("3. {:?}", ExpressionParser::parse(Rule::expression,  r###"1 + 2"###).unwrap());
+    eprintln!("4. {:?}", ExpressionParser::parse(Rule::expression,  r###"(1 + 3)"###).unwrap());
+    eprintln!("5. {:?}", ExpressionParser::parse(Rule::expression,  r###"1 + (4)"###).unwrap());
+    eprintln!("6. {:?}", ExpressionParser::parse(Rule::expression,  r###"1 + (5 + 6)"###).unwrap());
+    eprintln!("7. {:?}", ExpressionParser::parse(Rule::expression,  r###"(1 + 7) + (8 - 9)"###).unwrap());
+    eprintln!("8. {:?}", ExpressionParser::parse(Rule::expression,  r###"(1 + (2 + (3 + 4))) + 8"###).unwrap());
+    eprintln!("9. {:?}", ExpressionParser::parse(Rule::expression,  r###""hahaha""###).unwrap());
+    eprintln!("10. {:?}", ExpressionParser::parse(Rule::expression, r###""1 + 2""###).unwrap());
+    eprintln!("11. {:?}", ExpressionParser::parse(Rule::expression, r###""(3)""###).unwrap());
+    eprintln!("12. {:?}", ExpressionParser::parse(Rule::expression, r###"'aaaa'"###).unwrap());
+    eprintln!("13. {:?}", ExpressionParser::parse(Rule::expression, r###"'1 + 2'"###).unwrap());
+    eprintln!("14. {:?}", ExpressionParser::parse(Rule::expression, r###"'(3)'"###).unwrap());
+    eprintln!("15. {:?}", ExpressionParser::parse(Rule::expression, r###""'''''""###).unwrap());
+    eprintln!("16. {:?}", ExpressionParser::parse(Rule::expression, r###"'"""""'"###).unwrap());
+    eprintln!("17. {:?}", ExpressionParser::parse(Rule::expression, r###"id1"###).unwrap());
+    eprintln!("18. {:?}", ExpressionParser::parse(Rule::expression, r###"_id2"###).unwrap());
+    eprintln!("19. {:?}", ExpressionParser::parse(Rule::expression, r###"id_3"###).unwrap());
+    eprintln!("20. {:?}", ExpressionParser::parse(Rule::expression, r###"a + b + c + d + e"###).unwrap());
 }
